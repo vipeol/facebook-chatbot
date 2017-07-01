217d309df89a05e1cdcc6361833953f2a643f514
Vinicius Pedrosa <vinicius.pedrosa@gmail.com>
2017-07-01 14:49:14 -0300

    Adicionado os exemplos de botoes e listas

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use CodeBot\CallSendApi;
 use CodeBot\Element\Button;
+use CodeBot\Element\Product;
 use CodeBot\Message\Audio;
 use CodeBot\Message\File;
 use CodeBot\Message\Text;
@@ -11,6 +12,8 @@ use CodeBot\Message\Image;
 use CodeBot\Message\Video;
 use CodeBot\SenderRequest;
 use CodeBot\TemplatesMessage\ButtonsTemplate;
+use CodeBot\TemplatesMessage\GenericTemplate;
+use CodeBot\TemplatesMessage\ListTemplate;
 use CodeBot\WebHook;
 use Illuminate\Http\Request;
 
@@ -54,6 +57,26 @@ class BotController extends Controller
         $message->add(new Button('web_url','Code.Education','http://code.education'));
         $message->add(new Button('web_url','Google','https://www.google.com.br'));
         $callSendApi->make($message->message('Que tal testarmos a abertura de um site?'));
+
+        $button = new Button('web_url', null, 'https://angular.io/');
+        $product = new Product('Produto 1','https://www.w3schools.com/angular/pic_angular.jpg','Curso de Angular', $button);
+        $button = new Button('web_url', null, 'http://php.net/');
+        $product2 = new Product('Produto 2','https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/711px-PHP-logo.svg.png','Curso de PHP', $button);
+
+        $template = new GenericTemplate($senderId);
+        $template->add($product);
+        $template->add($product2);
+        $callSendApi->make($template->message('qwe'));
+
+        $button = new Button('web_url', null, 'https://angular.io/');
+        $product = new Product('Produto 1','https://www.w3schools.com/angular/pic_angular.jpg','Curso de Angular', $button);
+        $button = new Button('web_url', null, 'http://php.net/');
+        $product2 = new Product('Produto 2','https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/711px-PHP-logo.svg.png','Curso de PHP', $button);
+
+        $template = new ListTemplate($senderId);
+        $template->add($product);
+        $template->add($product2);
+        $callSendApi->make($template->message('qwe'));
 
         return '';
     }
